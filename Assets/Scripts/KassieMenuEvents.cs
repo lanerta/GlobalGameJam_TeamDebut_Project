2db844798472123a221496e177762676f09f56ff
kassieohern13 <kohern2022@gmail.com>
2023-02-05 11:48:01 -0500

    Title/Start Button

---
+++ Assets/Scripts/KassieMenuEvents.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KassieMenuEvents : MonoBehaviour
+{
+    public void LoadStart(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
