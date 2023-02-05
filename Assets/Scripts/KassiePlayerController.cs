f2f1186380ae34c7d1944af706c55a9476883c8b
lanerta <lgomezfr@outlook.com>
2023-02-04 19:20:56 -0500

    NewTag

--- Assets/Scripts/KassiePlayerController.cs
+++ Assets/Scripts/KassiePlayerController.cs
@@ -8,6 +8,10 @@ public class KassiePlayerController : MonoBehaviour
     public float moveSpeed = 10f;
 
     public Rigidbody2D _rigidbody2d;
+    private SpriteRenderer _spriteRenderer;
+    private bool isFacingLeft;
+    [SerializeField] private Sprite upSprite;
+    [SerializeField] private Sprite downSprite;
 
     private float moveX;
     
@@ -15,12 +19,33 @@ public class KassiePlayerController : MonoBehaviour
     void Awake()
     {
         _rigidbody2d = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
         moveX = Input.GetAxis("Horizontal") * moveSpeed;
+
+        if (moveX > 0)
+        {
+            isFacingLeft = false;
+        }
+        if (moveX < 0)
+        {
+            isFacingLeft = true;
+        }
+        _spriteRenderer.flipX = isFacingLeft;
+
+        if (_rigidbody2d.velocity.y >= 0)
+        {
+            _spriteRenderer.sprite = upSprite;
+        }
+        else
+        {
+            _spriteRenderer.sprite = downSprite;
+        }
     }
 
     private void FixedUpdate()
